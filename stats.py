781dd09ab5381f332c4049f50086ee19ecb672fd
aewchen <aewchen@gmail.com>
2025-07-25 23:36:33 -0700

    chapter3 completed

--- stats.py
+++ stats.py
@@ -17,4 +17,9 @@ def get_characters_dict(content):
         characters[char] = 1
   return characters
 
-
+def get_sorted_list(characters):
+  #sorted_dict = characters.sort(reverse=True, key= lambda x: x[
+  sorted_list = sorted(characters.items(), reverse=True, key=lambda item: item[1])
+  filtered_string_list = [ f"{values[0]}: {values[1]}" for values in sorted_list if values[0].isalpha()]
+  final_string = "\n".join(filtered_string_list)
+  return final_string
